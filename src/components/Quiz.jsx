5388ff26fbb319490f9929edac8b47207acc68cd
Rohit <rohitghadge348@gmail.com>
2025-10-03 22:08:10 +0530

    Added shuffle answers logic And fix quiz complete bug

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompeteImg from "../assets/quiz-complete.png";
+
 
 export default function Quiz(){
     const [userAnswers, setUserAnswers] = useState([]);
     
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
     
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswer) => {
@@ -13,12 +16,25 @@ export default function Quiz(){
         console.log(userAnswers);
     }
 
+    if(quizIsComplete){
+        return (
+            <div id="summary">
+                <img src={quizCompeteImg} alt="Trophy Icon" />
+                <h2>Quiz is complete</h2>
+            </div>
+        )
+    }
+    
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]; 
+    shuffledAnswers.sort(()=> Math.random() - 0.5);
+
+
     return (
         <div id="quiz">
             <div id="questions">
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
             <ul id="answers">
-                {QUESTIONS[activeQuestionIndex].answers.map((answer)=>(
+                {shuffledAnswers.map((answer)=>(
                     <li key={answer} className="answer">
                         <button onClick={()=>handleSelectAnswer(answer)}>
                            {answer}
